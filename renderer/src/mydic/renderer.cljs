681dfd10bd47ca392a8bd9d5f72ad7f4e5899552
hyperthreading <no1expman@gmail.com>
2018-01-20 17:55:00 +0900

    Advanced example

--- renderer/src/mydic/renderer.cljs
+++ renderer/src/mydic/renderer.cljs
@@ -14,27 +14,62 @@
  (fn [db [_ new-name]]
    (assoc db :name new-name)))
 
+(rf/reg-event-db
+ :time
+ (fn [db [_ new-time]]
+   (assoc db :time new-time)))
+
+(rf/reg-event-db
+ :change-time-color
+ (fn [db [_ new-color]]
+   (assoc db :time-color new-color)))
+
 (rf/reg-sub
  :name
  (fn [db _]
    (:name db)))
 
+(rf/reg-sub
+ :time
+ (fn [db _]
+   (:time db)))
+
+(rf/reg-sub
+ :time-color
+ (fn [db _]
+   (:time-color db)))
+
 (defn name-caller [name]
-  [:h2 {:style {:display "block"}} name "!"])
+  [:h2 {:style {:display "inline"}} name "!"])
 
 (defn hello []
   [:div "Hello, " [#'name-caller @(rf/subscribe [:name])]])
 
+(defn timer []
+  [:h1 {:style {:color @(rf/subscribe [:time-color])}}
+   "This is now " (.toTimeString @(rf/subscribe [:time]))])
+
+(defn timer-color-picker []
+  [:input {:type "text"
+           :value @(rf/subscribe [:time-color])
+           :on-change #(rf/dispatch [:change-time-color (-> % .-target .-value)])}])
+
 (defn app []
-  [#'hello])
+  [:div
+   [#'hello]
+   [#'timer]
+   [#'timer-color-picker]])
+
+(defn render []
+  (reagent/render
+   [#'app]
+   (.getElementById js/document "app")))
 
 (defn ^:export run []
-  (let [elem (.getElementById js/document "app")]
-    (rf/dispatch-sync [:init])
-    (reagent/render
-     [#'app]
-     elem)))
+  (rf/dispatch-sync [:init])
+  (render))
 
 (defn ^:export on-reload []
   (println "reloaded!"))
 
+(defonce timer-update (js/setInterval #(rf/dispatch [:time (js/Date.)]) 1000))
